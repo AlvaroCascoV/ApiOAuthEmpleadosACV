74f6d0cccf7bb535581c03e29f470ef88dec9a63
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-04-08 10:02:03 +0200

    login y logout con claims y auth

--- MvcOAuthApiEmpleados/Controllers/ManagedController.cs
+++ MvcOAuthApiEmpleados/Controllers/ManagedController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using MvcOAuthApiEmpleados.Models;
 using MvcOAuthApiEmpleados.Services;
+using System.Security.Claims;
 
 namespace MvcOAuthApiEmpleados.Controllers
 {
@@ -23,13 +26,31 @@ namespace MvcOAuthApiEmpleados.Controllers
             if (token == null)
             {
                 ViewData["MENSAJE"] = "Usuario o contraseña incorrectos";
+                return View();
             }
             else
             {
                 ViewData["MENSAJE"] = "Ya tienes tu Token!!";
                 HttpContext.Session.SetString("TOKEN", token);
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                //ALMACENAMOS EL NOMBRE DEL USUARIO PARA EL DIBUJO
+                identity.AddClaim(new Claim(ClaimTypes.Name, model.UserName));
+                //ALMACENAMOS EL PASSWORD DEL USUARIO COMO IDENTIFIER
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, model.Password));
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                //DAMOS DE ALTA AL USUARIO DURANTE 20 MINUTOS (como en el API)
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, 
+                    principal, new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                    });
+                return RedirectToAction("Index", "Home");
             }
-                return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
