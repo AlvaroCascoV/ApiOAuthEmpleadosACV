c85b78aaf8055de60302cd5b207fee7935cb18a2
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-04-08 09:54:37 +0200

    seguridad pre-claims

---
+++ MvcOAuthApiEmpleados/Controllers/ManagedController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcOAuthApiEmpleados.Models;
+using MvcOAuthApiEmpleados.Services;
+
+namespace MvcOAuthApiEmpleados.Controllers
+{
+    public class ManagedController : Controller
+    {
+        private ServiceEmpleados service;
+        public ManagedController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            string token = await this.service.LogInAsync(model.UserName, model.Password);
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "Usuario o contraseña incorrectos";
+            }
+            else
+            {
+                ViewData["MENSAJE"] = "Ya tienes tu Token!!";
+                HttpContext.Session.SetString("TOKEN", token);
+            }
+                return View();
+        }
+    }
+}
---
+++ MvcOAuthApiEmpleados/Models/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcOAuthApiEmpleados.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Apellido { get; set; }
+        public string Oficio { get; set; }
+        public int Salario { get; set; }
+        public int IdDepartamento { get; set; }
+    }
+}
